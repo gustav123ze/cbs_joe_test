e1f6255028bbfad440e6e7acdecb0441d242ab7b
gustav123ze <118443006+gustav123ze@users.noreply.github.com>
2023-09-20 09:24:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hej din nørd\n');
+  res.end('Hej det virker\n');
 });
 
 server.listen(port, hostname, () => {
