a82c19eaa9dc0da7189f7d2c3e71bb0babf35861
gustav123ze <guze22ab@student.cbs.dk>
2023-09-22 22:53:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(' 123123 \n');
+  res.end(' 12312314214214 \n');
 });
 
 server.listen(port, hostname, () => {
