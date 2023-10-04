aaa45b925893902b79dadd6339a974f5e971ddc2
gustav123ze <guze22ab@student.cbs.dk>
2023-10-04 08:16:11 +0200

    ret

--- app.js
+++ app.js
@@ -1,44 +1,12 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const http = require("http");
-const socketIo = require("socket.io");
-const path = require("path");
-
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; // Du kan ændre porten efter behov
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const server = http.createServer(app); // Opret en HTTP-server
-const io = socketIo(server); // Konfigurér Socket.IO
-
-const host = "0.0.0.0"; // Lyt på alle tilgængelige IP-adresser
-
-app.use(express.static(path.join(__dirname, "./client")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/home.html"));
+app.get('/', (req, res) => {
+  res.send('Hej');
 });
 
-app.get("/global.css", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/global.css"));
+app.listen(port, () => {
+  console.log(`Server kører på http://localhost:${port}`);
 });
-
-app.get("/home.js", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/home.js"));
-});
-
-server.listen(port, host, () => {
-  console.log(`Express server running at http://${host}:${port}/`);
-});
-
-io.on("connection", (socket) => {
-  console.log("Someone connected");
-  socket.on("chat message", (msg) => {
-    console.log("Message: " + msg);
-    io.emit("chat message", msg);
-  });
-});
-
 
