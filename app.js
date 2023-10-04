5f57344e9d8c6bf80664fa156ba5a2cb607619c4
gustav123ze <guze22ab@student.cbs.dk>
2023-10-04 21:32:38 +0200

    test

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000; // Du kan ændre porten efter behov
 
 app.get('/', (req, res) => {
-  res.send('Hej');
-});
+  res.send('Hej det virker test');
+}); 
 
 app.listen(port, () => {
   console.log(`Server kører på http://localhost:${port}`);
