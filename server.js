8f463a56a5511fa840b91a97cbb00d2fb0a7fee3
gustav123ze <guze22ab@student.cbs.dk>
2023-10-09 15:32:56 +0200

    ret

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 app.get('/testapi', (req, res) => {
-    res.status(200).send('test det virker');
+    res.status(200).send('test det virker!');
 });
 
 app.use((req, res) => {
