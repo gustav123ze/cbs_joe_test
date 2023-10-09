38ac839009c3b33ce38a47abeb939a477bf34948
gustav123ze <guze22ab@student.cbs.dk>
2023-10-09 15:12:31 +0200

    ret

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('test');
+    if (req.url === '/testapi' && req.method === 'GET') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('test det virker');
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
